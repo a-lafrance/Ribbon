38225023168cc3e67fed66f7f41be099afe5ff3c
Ryan Mohta <ryanmohta@gmail.com>
2021-01-31 02:46:28 -0800

    Changed div name from hero to container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -62,7 +62,7 @@ export default App;
 function Home(props) {
     return (
         <div className="App">
-          <div className="hero">
+          <div className="container">
             <h1>Groupchat <span className="bolded">Wrapped</span></h1>
             <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
             <StyledDropzone onFileInput={props.onFileInput}/>
