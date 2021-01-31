b35a0888938051aed66eec500807050d58abe7ce
Ryan Mohta <ryanmohta@gmail.com>
2021-01-31 00:43:25 -0800

    Created StepPanel component

--- frontend/src/components/Tutorial.js
+++ frontend/src/components/Tutorial.js
@@ -10,31 +10,25 @@ let Tutorial = props => {
     return(
         <div className="tutorialPanel"> 
             <h1> How to Export Your Messenger Data</h1>
-            <div className="stepPanelH"> 
+            <div className="stepPanelH">
                 <span> Go to</span>
                 <a href="https://www.facebook.com/dyi">https://www.facebook.com/dyi  </a>
             </div>
-            <div className="stepPanelH"> 
-                <div> 
-                    "Deselect All" and then select "Messages"
-                </div>
-                <div><img className="stepImg" src={step1}/></div> 
-            </div>
-            <div className="stepPanelH"> 
-                <div> "Create File" with your preferred date and settings shown below</div>
-                <div><img className="stepImg" src={step2}/></div> 
-            </div>
-            <div className="stepPanelH"> 
-                <div> Download the file when it is ready (this can take upwards of 20 minutes, depending on your date settings)</div>
-                <div><img className="stepImg" src={step3}/></div> 
-            </div>
-            <div className="stepPanelH"> 
-                <div> Extract the file and find the json of the chat to analyze (facebook-name > messages > inbox > chatname > message_1.json)</div>
-                <div><img className="stepImg" src={step4}/></div> 
-            </div>
-
+            <StepPanel description='"Deselect All" and then select "Messages"' image={step1} />
+            <StepPanel description='"Create File" with your preferred date and settings shown below' image={step2} />
+            <StepPanel description='Download the file when it is ready (this can take upwards of 20 minutes, depending on your date settings)' image={step3} />
+            <StepPanel description='Extract the file and find the json of the chat to analyze (facebook-name > messages > inbox > chatname > message_1.json)' image={step4} />
         </div>
     );
+}
+
+function StepPanel(props) {
+  return (
+    <div className="stepPanelH">
+        <div>{props.description}</div>
+        <div><img className="stepImg" src={props.image}/></div>
+    </div>
+  )
 }
 
 export default Tutorial;
