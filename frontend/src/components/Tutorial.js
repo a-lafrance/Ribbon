41f0412631cb3c529cd3358d20518dc1899e3480
Ryan Mohta <ryanmohta@gmail.com>
2021-01-31 00:56:22 -0800

    Added step number

--- frontend/src/components/Tutorial.js
+++ frontend/src/components/Tutorial.js
@@ -17,18 +17,20 @@ class Tutorial extends React.Component {
   }
 
   render() {
+    let steps = [
+      <StepPanel description='"Deselect All" and then select "Messages"' image={step1} />,
+      <StepPanel description='"Create File" with your preferred date and settings shown below' image={step2} />,
+      <StepPanel description='Download the file when it is ready (this can take upwards of 20 minutes, depending on your date settings)' image={step3} />,
+      <StepPanel description='Extract the file and find the json of the chat to analyze (facebook-name > messages > inbox > chatname > message_1.json)' image={step4} />
+    ]
+
     return(
       <div className="tutorialPanel">
         <h1> How to Export Your Messenger Data</h1>
       <div className="stepPanelH">
         <span> Go to</span>
         <a href="https://www.facebook.com/dyi">https://www.facebook.com/dyi</a>
-      </div>
-        <StepPanel description='"Deselect All" and then select "Messages"' image={step1} />
-        <StepPanel description='"Create File" with your preferred date and settings shown below' image={step2} />
-        <StepPanel description='Download the file when it is ready (this can take upwards of 20 minutes, depending on your date settings)' image={step3} />
-        <StepPanel description='Extract the file and find the json of the chat to analyze (facebook-name > messages > inbox > chatname > message_1.json)' image={step4} />
-      </div>
+      </div>{steps[this.state.stepNumber - 1]}</div>
     );
   }
 
