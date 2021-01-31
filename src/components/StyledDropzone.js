fa51af108368a411ab17aa34672bd11142f44ed8
Kevin Xu <bobthehoboofhobos@gmail.com>
2021-01-30 19:42:47 -0800

    added initial json input processing

--- src/components/StyledDropzone.js
+++ src/components/StyledDropzone.js
@@ -21,7 +21,18 @@ const activeStyle = {
     borderColor: '#2196f3'
 };
 
-
+function processFileInput(files) {
+    console.log(files);
+    const file = files[0]; 
+    const fr = new FileReader();
+    fr.onload = function (e) {
+        const content = JSON.parse(e.target.result);
+        //processContent(content)
+        // Import Arthur's js processing and call it here
+        console.log(content);
+    }
+    fr.readAsText(file);
+}
 
 function StyledDropzone(props) {
     const {
@@ -30,7 +41,7 @@ function StyledDropzone(props) {
         isDragActive,
     } = useDropzone({
         accept: '.json',
-        onDrop: files => console.log(files),
+        onDrop: files => processFileInput(files),
     });
 
     const style = useMemo(() => ({
